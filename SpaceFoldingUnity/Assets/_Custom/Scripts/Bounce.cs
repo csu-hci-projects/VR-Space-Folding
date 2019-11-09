6342a3ed891fd38248caf06f0b884b2250d48340
JoeStrout <joe@strout.net>
2019-11-08 17:14:58 -0700

    Updated project with new models and more work on the 4x3 layout.

---
+++ SpaceFoldingUnity/Assets/_Custom/Scripts/Bounce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+	public Vector3 bounceOffset = Vector3.up;
+	public float bounceTime = 1;
+
+	Vector3 startPos;
+	
+	protected void Start() {
+		startPos = transform.position;
+	}
+	
+	protected void Update() {
+		transform.position = startPos + bounceOffset 
+			* Mathf.Abs(Mathf.Cos(Time.time/bounceTime * Mathf.PI));
+	}
+	
+}
