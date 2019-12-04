e4fd793ca1287fa1e3a937f4734ac2a81ef6596d
JoeStrout <joe@strout.net>
2019-12-04 13:17:26 -0700

    Added hand tracking and updated Oculus plugin.

---
+++ SpaceFoldingUnity/Assets/_Custom/Scripts/HandTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandTracker : MonoBehaviour {
+
+	public enum Hand {
+		Left,
+		Right
+	}
+	//	public Hand hand = Hand.Right;
+	public OVRInput.Controller controller;
+
+	protected void Update() {
+		//		OVRInput.Controller c = hand == Hand.Left ? OVRInput.Controller.LTouch : OVRInput.Controller.RTouch;
+		transform.localRotation = OVRInput.GetLocalControllerRotation(controller);
+		transform.localPosition = OVRInput.GetLocalControllerPosition(controller);
+		Debug.Log(gameObject.name + " at " + transform.localPosition + " (cam at " + Camera.main.transform.position + ")");
+	}
+}
